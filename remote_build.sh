6335b316aa70e7be2514a52dc0b0a8552c4ab0e7
imranpopz <imranpopz32@gmail.com>
2020-08-05 16:00:24 +0530

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -161,11 +161,11 @@ if [[ "${TEST_BUILD}" = "true" ]]; then
     echo "Got the Unofficial Build: $TEST_BUILDFILE"
     export TEST_BUILDIMG=$(find $(pwd)/out/target/product/${CODENAME}/recovery.img 2>/dev/null)
     if [ "$USE_SECRET_BOOTABLE" = 'true' ]; then
-    cp $TEST_BUILDIMG recovery.img
+    cp ${TEST_BUILDIMG} recovery.img
     TEST_IT=$(curl -F'file=@recovery.img' https://0x0.st)
     else
-    cp $TEST_BUILDFILE $UPLOAD_PATH
-    cp $TEST_BUILDIMG $UPLOAD_PATH
+    cp ${TEST_BUILDFILE} ${UPLOAD_PATH}
+    cp ${TEST_BUILDIMG} ${UPLOAD_PATH}
     ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Test Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION}-test ${UPLOAD_PATH}
     fi
 else
