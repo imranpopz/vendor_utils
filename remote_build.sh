3eff53991f4c4d48322486047bef1344676e3279
imranpopz <imranpopz32@gmail.com>
2020-08-06 09:39:23 +0530

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -72,6 +72,8 @@ echo -e "Starting the CI Build Process...\n"
 curl -sL https://gist.github.com/rokibhasansagar/cf8669411a1a57ba40c3090cd5146cd9/raw/keepalive.sh -o /tmp/keepalive.sh
 chmod a+x /tmp/keepalive.sh
 
+mkdir "$UPLOAD_PATH"
+
 # sync
 echo -e "Initializing PBRP repo sync..."
 repo init -q -u https://github.com/PitchBlackRecoveryProject/manifest_pb.git -b ${MANIFEST_BRANCH} --depth 1
