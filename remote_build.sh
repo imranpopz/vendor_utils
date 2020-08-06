b781db73e9b17634cff08bafc1f5bd0222711ba9
imranpopz <imranpopz32@gmail.com>
2020-08-06 23:37:58 +0530

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -139,10 +139,10 @@ echo -e "\nYummy Recovery is Served.\n"
 mkdir UPLOAD_PATH
 
 echo "Ready to Deploy"
-export TEST_BUILDFILE="$(find $(pwd)/out/target/product/${CODENAME}/PBRP*-UNOFFICIAL.zip 2>/dev/null)"
-export BUILDFILE="$(find $(pwd)/out/target/product/${CODENAME}/PBRP*-OFFICIAL.zip 2>/dev/null)"
-export BUILD_FILE_TAR="$(find $(pwd)/out/target/product/${CODENAME}/*.tar 2>/dev/null)"
-#export UPLOAD_PATH="$(pwd)/out/target/product/${CODENAME}/upload/"
+export TEST_BUILDFILE=$(find $(pwd)/out/target/product/${CODENAME}/PBRP*-UNOFFICIAL.zip 2>/dev/null)
+export BUILDFILE=$(find $(pwd)/out/target/product/${CODENAME}/PBRP*-OFFICIAL.zip 2>/dev/null)
+export BUILD_FILE_TAR=$(find $(pwd)/out/target/product/${CODENAME}/*.tar 2>/dev/null)
+#export UPLOAD_PATH=$(pwd)/out/target/product/${CODENAME}/upload/
 
 if [ -n "${BUILD_FILE_TAR}" ]; then
   echo "Samsung's Odin Tar available: $BUILD_FILE_TAR"
@@ -158,14 +158,14 @@ fi
  #   cp $BUILDFILE $UPLOAD_PATH
  #   ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Latest Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION} ${UPLOAD_PATH}
 if [[ "${TEST_BUILD}" = "true" ]]; then
-    echo "Got the Unofficial Build: $TEST_BUILDFILE"
+    echo "Got the Unofficial Build: ${TEST_BUILDFILE}"
     export TEST_BUILDIMG=$(find $(pwd)/out/target/product/${CODENAME}/recovery.img 2>/dev/null)
     if [ "$USE_SECRET_BOOTABLE" = 'true' ]; then
     cp "${TEST_BUILDIMG}" recovery.img
     TEST_IT=$(curl -F'file=@recovery.img' https://0x0.st)
     else
-    cp $TEST_BUILDFILE $UPLOAD_PATH
-    cp $TEST_BUILDIMG $UPLOAD_PATH
+    cp $TEST_BUILDFILE UPLOAD_PATH
+    cp $TEST_BUILDIMG UPLOAD_PATH
     ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Test Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION}-test ${UPLOAD_PATH}
     fi
 else
