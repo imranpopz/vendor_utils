0c30a49a9b3cbe727d55e69476ca6837774fa387
imranpopz <imranpopz32@gmail.com>
2020-08-06 21:42:00 +0530

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -164,8 +164,8 @@ if [[ "${TEST_BUILD}" = "true" ]]; then
     cp "${TEST_BUILDIMG}" recovery.img
     TEST_IT=$(curl -F'file=@recovery.img' https://0x0.st)
     else
-    cp $TEST_BUILDFILE UPLOAD_PATH
-    cp $TEST_BUILDIMG UPLOAD_PATH
+    cp $TEST_BUILDFILE $UPLOAD_PATH
+    cp $TEST_BUILDIMG $UPLOAD_PATH
     ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Test Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION}-test ${UPLOAD_PATH}
     fi
 else
