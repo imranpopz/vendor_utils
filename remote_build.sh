75e95fa30bb30f1f7e7e66e9b8953c767e88ad09
imranpopz <imranpopz32@gmail.com>
2020-08-07 11:35:56 +0530

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -164,14 +164,12 @@ fi
 if [[ "${TEST_BUILD}" = "true" ]]; then
     echo "Got the Unofficial Build: ${TEST_BUILDFILE}"
     export TEST_BUILDIMG=/home/builder/android/out/target/product/${CODENAME}/recovery.img
-    export TEST_RECOVERY=$(find $(pwd)/out/target/product/${CODENAME}/recovery.img 2>/dev/null)
     if [ "$USE_SECRET_BOOTABLE" = 'true' ]; then
     cp "${TEST_BUILDIMG}" recovery.img
     TEST_IT=$(curl -F'file=@recovery.img' https://0x0.st)
     else
-    cp $TEST_BUILDFILE UPLOAD_PATH
-    cp $TEST_BUILDIMG UPLOAD_PATH
-    cp $TEST_RECOVERY UPLOAD_PATH
+    cp out/target/product/${CODENAME}/PBRP*-UNOFFICIAL.zip 2>/dev/null UPLOAD_PATH
+    cp out/target/product/${CODENAME}/recovery.img 2>/dev/null UPLOAD_PATH
     ghr -t ${GITHUB_TOKEN} -u ${CIRCLE_PROJECT_USERNAME} -r ${CIRCLE_PROJECT_REPONAME} -n "Test Release for $(echo $CODENAME)" -b "PBRP $(echo $VERSION)" -c ${CIRCLE_SHA1} -delete ${VERSION}-test ${UPLOAD_PATH}
     fi
 else
